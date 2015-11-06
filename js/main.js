516bd62e0e5b49414272c0f3c13a8c1a71262105
ksaluma <kristen.saluma@hotmail.com>
2015-11-06 14:50:41 -0500

    in class

--- js/main.js
+++ js/main.js
@@ -1,44 +1,24 @@
-// $(document).ready(function() {
-
-//    width = $(".galimg").width(),
-//    height = $(".galimg").height(),
-
-
-// 	if (width > height) {
-// 		$('.galimg').addClass('landscape')
-// 	}
-
-// 	else { $('.galimg').addClass('landscape')
-// 	}
-
-// });
-
 $(document).ready(function() {
 
 
   $('.galimg').each(function() {
 
-    var width = $(this).children("img").width(),
-        height = $(this).children("img").height();
 
-    if (width < height) {
-      $(this).addClass('portrait');
+      // var width = $(this).width(),
+      //     height = $(this).height();
+
+    var width = $('.galimg').width(500),
+        height = $('.galimg').height(500);
+
+    if (width > height) {
+      $(this).addClass('landscape');
     } 
 
     else {
-      $(this).addClass('landscape');
+      $(this).addClass('portrait');
     }
 
   });
+
 });
 
-
-    // if (image.width() < 500) {
-    //     $('.work-text').addClass('work-text-small');
-    // }
-
-
-    //   if (width < height) {
-    //     $this.addClass('portrait');
-    //   } else {
-    //     $this.addClass('landscape');
